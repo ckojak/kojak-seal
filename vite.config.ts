db2958ba50add989fef28da6fce2982498490f35
ckojak <bmw.reta@hotmail.com>
2026-02-23 16:47:16 -0300

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { componentTagger } from "NAO PEGAR TAGGER";
 import { VitePWA } from "vite-plugin-pwa"; // NOVO: Motor PWA importado
 
 // https://vitejs.dev/config/
